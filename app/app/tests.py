6a372cd3b07a2a60a34fdc430b7ab38f330eae0c
Pedro Ivo de Faria Ramos <pedroivo@Pedros-MacBook-Pro.local>
2022-10-05 18:28:32 -0300

    Fixing flake8 lint problems

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,17 +6,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-  """Test the calc module."""
+    """Test the calc module."""
 
-  def test_add_numbers(self):
-    """Test adding numbers."""
-    res = calc.add(5, 6)
+    def test_add_numbers(self):
+        """Test adding numbers."""
+        res = calc.add(5, 6)
 
-    self.assertEqual(res, 11)
+        self.assertEqual(res, 11)
 
-  def test_subtract_numbers(self):
-    """Test subtracting numbers. """
-    res = calc.subtract(15, 10)
+    def test_subtract_numbers(self):
+        """Test subtracting numbers. """
+        res = calc.subtract(15, 10)
 
-    self.assertEqual(res, 5)
+        self.assertEqual(res, 5)
